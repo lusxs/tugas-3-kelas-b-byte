26bf2fb04bc8f6aea6ce7b7f8bc6000a39ef0895
lusxs <lusiakaparang8@gmail.com>
2022-11-16 15:07:35 +0800

    fifth commit

---
+++ routes/MahesaRoute.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import { 
+    createMahesa, 
+    deleteMahesa, 
+    getMahesa, 
+    getMahesaById, 
+    updateMahesa 
+} from '../controller/mahesa.js';
+
+const router  = express.Router();
+
+router.get('/mahesas', getMahesa);
+router.get('/mahesa/:id', getMahesaById);
+router.post('./mahesa', createMahesa);
+router.patch('/mahesa/:id', updateMahesa);
+router.delete('/mahesa/:id', deleteMahesa);
+
+export default router;
